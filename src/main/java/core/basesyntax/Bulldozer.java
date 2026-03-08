1a5e44e9db2114c2b609cc08191faf668041546d
AlexPranschke <alexpranschke@gmail.com>
2026-03-08 13:16:58 +0100

    poprawa machinne.java

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -4,7 +4,6 @@ public class Bulldozer extends Machine {
     public void doWork() {
         System.out.println("Bulldozer started working");
     }
-
     public void stopWork() {
         System.out.println("Bulldozer stopped working");
     }
--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -4,7 +4,6 @@ public class Excavator extends Machine {
     public void doWork() {
         System.out.println("Excavator started working");
     }
-
     public void stopWork() {
         System.out.println("Excavator stopped working");
     }
